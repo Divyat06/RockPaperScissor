0c0f08c551c1319b69efc1bed155932c9096a918
Divyat06 <137271530+Divyat06@users.noreply.github.com>
2023-10-02 20:17:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,137 @@
+// Prevent animation on load
+setTimeout(() => {
+  document.body.classList.remove("preload");
+}, 500);
+
+// DOM
+const btnRules = document.querySelector(".rules-btn");
+const btnClose = document.querySelector(".close-btn");
+const modalRules = document.querySelector(".modal");
+
+const CHOICES = [
+  {
+    name: "paper",
+    beats: "rock",
+  },
+  {
+    name: "scissor",
+    beats: "paper",
+  },
+  {
+    name: "rock",
+    beats: "scissor",
+  },
+];
+const choiceButtons = document.querySelectorAll(".choice-btn");
+const gameDiv = document.querySelector(".game");
+const resultsDiv = document.querySelector(".results");
+const resultDivs = document.querySelectorAll(".results_result");
+
+const resultWinner = document.querySelector(".results_winner");
+const resultText = document.querySelector(".results_text");
+
+const playAgainBtn = document.querySelector(".play-again");
+
+const scoreNumber = document.querySelector(".score_number");
+const scoreNumberComputer=document.querySelector(".score_number_computer")
+let score = 0;
+let scoreComputer=0;
+// Game Logic
+choiceButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const choiceName = button.dataset.choice;
+    const choice = CHOICES.find((choice) => choice.name === choiceName);
+    choose(choice);
+  });
+});
+
+function choose(choice) {
+  const aichoice = aiChoose();
+  displayResults([choice, aichoice]);
+  displayWinner([choice, aichoice]);
+}
+
+function aiChoose() {
+  const rand = Math.floor(Math.random() * CHOICES.length);
+  return CHOICES[rand];
+}
+
+function displayResults(results) {
+  resultDivs.forEach((resultDiv, idx) => {
+    setTimeout(() => {
+      resultDiv.innerHTML = `
+        <div class="choice ${results[idx].name}">
+          <img src="images/${results[idx].name}.jpg" alt="${results[idx].name}" />
+        </div>
+      `;
+    }, idx * 1000);
+  });
+
+  gameDiv.classList.toggle("hidden");
+  resultsDiv.classList.toggle("hidden");
+}
+
+function displayWinner(results) {
+  setTimeout(() => {
+    const userWins = isWinner(results);
+    const aiWins = isWinner(results.reverse());
+
+    if (userWins) {
+      resultText.innerText = "you win";
+      resultDivs[0].classList.toggle("winner");
+      keepScore(true);
+    } else if (aiWins) {
+      resultText.innerText = "you lose";
+      resultDivs[1].classList.toggle("winner");
+      keepScore(false);
+    } else {
+      resultText.innerText = "draw";
+    }
+    resultWinner.classList.toggle("hidden");
+    resultsDiv.classList.toggle("show-winner");
+  }, 1000);
+}
+
+function isWinner(results) {
+  return results[0].beats === results[1].name;
+}
+
+function keepScore(point) {
+  // score += point;
+  // scoreNumber.innerText = score;
+  if(point)
+  {
+    scoreComputer+=1;
+    scoreNumberComputer.innerText=scoreComputer;
+    
+  }
+  else 
+  {
+    score+=1;
+    scoreNumber.innerText=score;
+  }
+
+}
+
+// Play Again
+playAgainBtn.addEventListener("click", () => {
+  gameDiv.classList.toggle("hidden");
+  resultsDiv.classList.toggle("hidden");
+
+  resultDivs.forEach((resultDiv) => {
+    resultDiv.innerHTML = "";
+    resultDiv.classList.remove("winner");
+  });
+
+  resultText.innerText = "";
+  resultWinner.classList.toggle("hidden");
+  resultsDiv.classList.toggle("show-winner");
+});
+
+// Show/Hide Rules
+btnRules.addEventListener("click", () => {
+  modalRules.classList.toggle("show-modal");
+});
+btnClose.addEventListener("click", () => {
+  modalRules.classList.toggle("show-modal");
+});
